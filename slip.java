d4af5345c657a2d43968470b69242f3f09c2f299
iSAD <bradx2713@gmail.com>
2024-07-11 17:11:16 +0200

    Add files via upload

---
+++ slip.java
@@ -0,0 +1,66 @@
+package back;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class slip {
+
+	public static void main(String[] args) {
+		
+		LocalDate currentDate = LocalDate.now();
+		LocalTime currentTime = LocalTime.now();
+		
+		System.out.println("		DEBONAIRS CBD");
+		System.out.println("		  POLOKWANE");
+		System.out.println(currentDate + "			" + currentTime);
+		System.out.println("Terminal ID:			" + "0000J0I2");
+		System.out.println("Merchant ID:			" + "000000004493753");
+		System.out.println(""
+				+ ""
+				+ "");
+		System.out.println("Purchase");
+		System.out.println("Transaction #:			" + "8");
+		System.out.println("Card Type:			" + "Extended");
+		System.out.println("Account:			" + "739438290347***************65");
+		System.out.println("Description:			" + "STDB DEBIT MC");
+		System.out.println("Entry:				" + "chip");
+		System.out.println("ref . Number:			" + "000066849975");
+		System.out.println("Trace ID:			" + "000066849975");
+		System.out.println("Global UID:			" + "");
+		System.out.println("	1851624047202407081732535089");
+		System.out.println("STAN:			" + "000078");
+		System.out.println("Auth. Code:			" + "338900");
+		System.out.println(""
+				+ ""
+				+ "");
+		System.out.println("Batch #:			" + "194");
+		System.out.println("Response:			" + "0 - Approved");
+		System.out.println(""
+				+ ""
+				+ "");
+		System.out.println("Model:				" + "Issuer");
+		System.out.println("AID:				" + "A0000000041010");
+		System.out.println("TVR:				" + "0000048000");
+		System.out.println("CVR:			" + "");
+		System.out.println("0110607001220000207600000000");
+		System.out.println("000000FF");
+		System.out.println("TSI:				" + "E800");
+		System.out.println("RespCode:			" + "00");
+		System.out.println("AC:				" + "FAAFCE6A0E0C936A");
+		System.out.println("AIC:				" + "008C");
+		System.out.println("APPN:				" + "Debit Mastercard");
+		System.out.println(""
+				+ ""
+				+ "");
+		System.out.println("	" + "PIN VERIFIED");
+		System.out.println(""
+				+ ""
+				+ "");
+		System.out.println("Amount:				" + "R89.90");
+		System.out.println(""
+				+ ""
+				+ "");
+		System.out.println("	" + "Customer Copy");
+	}
+
+}
